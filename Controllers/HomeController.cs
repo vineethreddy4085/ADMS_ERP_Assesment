3c621786fdc5d697ad690cc5b58acf2e52bb59aa
vineethreddy4085 <120081539+vineethreddy4085@users.noreply.github.com>
2026-03-31 20:15:25 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,57 @@
 using System.Web.Mvc;
 using ItemProcessingApp.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ItemProcessingApp.Controllers
 {
     public class HomeController : Controller
     {
+        // Fake in-memory DB
+        static List<Item> items = new List<Item>();
+
         public ActionResult Index()
         {
-            var items = new List<Item>
-            {
-                new Item { Id = 1, Name = "Item1", Price = 100 },
-                new Item { Id = 2, Name = "Item2", Price = 200 }
-            };
-
             return View(items);
+        }
+
+        // Add Item
+        [HttpPost]
+        public ActionResult Add(string name, double weight)
+        {
+            items.Add(new Item
+            {
+                Id = items.Count + 1,
+                Name = name,
+                Weight = weight,
+                Children = new List<Item>()
+            });
+
+            return RedirectToAction("Index");
+        }
+
+        // Process Item → add child
+        [HttpPost]
+        public ActionResult Process(int parentId, string childName, double childWeight)
+        {
+            var parent = items.FirstOrDefault(x => x.Id == parentId);
+
+            if (parent != null)
+            {
+                var child = new Item
+                {
+                    Id = items.Count + 1,
+                    Name = childName,
+                    Weight = childWeight,
+                    ParentId = parentId,
+                    Children = new List<Item>()
+                };
+
+                parent.Children.Add(child);
+                items.Add(child);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
